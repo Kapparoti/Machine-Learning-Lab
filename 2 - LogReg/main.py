3fff8aea108ce15fd8b9acc8128d05de55b6d7b8
Kapparoti <matteosita003@gmail.com>
2025-07-20 18:26:05 +0200

    small improvements

--- 2 - LogReg/main.py
+++ 2 - LogReg/main.py
@@ -8,24 +8,24 @@ np.random.seed(191090)
 
 
 def main():
-    """ Main function """
+	""" Main function """
 
-    # x_train, y_train, x_test, y_test = gaussians_dataset(2, [100, 150], [[1, 3], [-4, 8]], [[2, 3], [4, 1]])
-    x_train, y_train, train_names, x_test, y_test, test_names, feature_names = load_got_dataset(path='data/got.csv', train_split=0.8)
+	# x_train, y_train, x_test, y_test = gaussians_dataset(2, [100, 150], [[1, 3], [-4, 8]], [[2, 3], [4, 1]])
+	x_train, y_train, train_names, x_test, y_test, test_names, feature_names = load_got_dataset(path='data/got.csv', train_split=0.8)
 
-    logistic_reg = LogisticRegression()
+	logistic_reg = LogisticRegression()
 
-    # train
-    logistic_reg.fit_gd(x_train, y_train, n_epochs=10000, learning_rate=0.01, verbose=True)
+	# train
+	logistic_reg.fit_gd(x_train, y_train, n_epochs=10000, learning_rate=0.01, verbose=True)
 
-    # test
-    predictions = logistic_reg.predict(x_test)
+	# test
+	predictions = logistic_reg.predict(x_test)
 
-    accuracy = float(np.sum(predictions == y_test)) / y_test.shape[0]
-    print(f'Test accuracy: {accuracy}')
+	accuracy = float(np.sum(predictions == y_test)) / y_test.shape[0]
+	print(f'Test accuracy: {accuracy}')
 
-    # plot_boundary(x_train, y_train, logistic_reg, title='Training Set')
+	# plot_boundary(x_train, y_train, logistic_reg, title='Training Set')
 
 # entry point
 if __name__ == '__main__':
-    main()
+	main()
